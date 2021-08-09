e626df931eea172131cce2c01c31bc591611771a
Maximenzo <maxime.nzorubara@gmail.com>
2021-08-09 15:54:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { firestore } from "./firebase"
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  
+  state = {
+    task:'',
+    user:''
+  }
+
+
+  updateInput = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  addTask = event => {
+    event.preventDefault()
+  
+    firestore.collection("test").add({
+      task: this.state.task,
+      user: this.state.user
+    })
+  
+    this.setState({ task: "", user: "" })
+  }
+
+  render() {
+    const {task, user} = this.state;
+    
+    return(
+      <Container maxWidth='sm' marginTop='auto' marginBottom='auto'>
+        <div className='App'>
+                <form onSubmit={this.addTask}>
+                <div style={{margin: '1rem'}}><TextField 
+                  id="outlined-basic" 
+                  label="Task" 
+                  variant="outlined" 
+                  name='task'
+                  onChange={this.updateInput}
+                  value={task} /></div>
+                
+                
+          <br />
+                <div style={{margin: '1rem'}}><TextField 
+                  id="outlined-basic" 
+                  label="User" 
+                  variant="outlined" 
+                  name='user'
+                  onChange={this.updateInput}
+                  value={user} /></div>
+          <br />
+                    <Button variant="contained" color="primary" type='submit'>Submit</Button>
+                </form>
+        </div>
+      </Container>
+    )
+  }
 }
 
 export default App;
